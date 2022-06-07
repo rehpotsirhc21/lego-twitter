a396f32f92ac8fde16d7e3ece2dcf87e1c2a010d
Christopher Langner <langnerc@icloud.com>
2022-06-06 20:26:09 -0500

    added bcrypt for log in and outline for models

---
+++ models/Like.js
@@ -0,0 +1,11 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Like extends Model{}
+
+Like.init(
+
+)
+
+
+module.exports = Like;
---
+++ models/index.js
@@ -0,0 +1,5 @@
+// import all models
+const Post = require('./Post');
+const User = require('./User');
+const Vote = require('./Like');
+const Comment = require('./Comment');
