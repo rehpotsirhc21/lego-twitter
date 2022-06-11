df3be87b913278c86562d5ebae6f47684b1cda87
Bob <rwillford0719@gmail.com>
2022-06-11 14:02:19 -0500

    added get all and get on to post routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 
 const userRoutes = require('./user');
-// const postRoutes = require('./post');
+const postRoutes = require('./post');
 const commentRoutes = require('./comment');
 
 router.use('/users', userRoutes);
-// router.use('/posts', postRoutes);
+router.use('/posts', postRoutes);
 router.use('/comments', commentRoutes);
 
 module.exports = router;
---
+++ controllers/api/post.js
@@ -0,0 +1,76 @@
+const router = require('express').Router();
+const sequelize = require('../../config/connection');
+const { User, Post, Comment, ProfilePic } = require('../../models');
+
+router.get('/', (req, res) => {
+    Post.findAll({
+        attributes: [
+            'id',
+            'title',
+            'post_body',
+            'created_at',
+            // [sequelize.literal('(SELECT COUNT(*) FROM like WHERE post.id = like.post_id)'), 'like_count']
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.get('/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'title',
+            'post_body',
+            'created_at',
+            [sequelize.literal('(SELECT COUNT(*) FROM like WHERE post.id = like.post_id)'), 'like_count']
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData => {
+        if(!dbPostData) {
+            res.status(404).json({ message: 'No post found with this ID!'})
+            return;
+        }
+        res.json(dbPostData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+module.exports = router;
