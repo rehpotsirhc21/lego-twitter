e577c7e5b49a5fbd17aac152d0479785ccb20de2
Bob <rwillford0719@gmail.com>
2022-06-11 14:35:03 -0500

    added all post routes

--- controllers/api/post.js
+++ controllers/api/post.js
@@ -97,6 +97,49 @@ router.put('/like', (req, res) => {
     }
 });
 
-router.put()
+router.put('/:id', (req, res) => {
+    Post.update(
+        {
+            title: req.body.title,
+            post_body: req.body.post_body
+        },
+        {
+            where: {
+                id: req.params.id
+            }
+        }
+
+    )
+    .then(dbPostData => {
+        if(!dbPostData) {
+            res.status(404).json({ message: 'No post found with this id!'})
+            return;
+        }
+        res.json(dbPostData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.delete('/:id', (req, res) => {
+    Post.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbPostData => {
+        if(!dbPostData) {
+            res.status(404).json({ message: 'No post with this id'});
+            return;
+        }
+        res.json(dbPostData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
 module.exports = router;
