82d9fd5e3c97c1839e48b0b6dfb2371660ca3f38
Bob <rwillford0719@gmail.com>
2022-06-11 13:18:42 -0500

    added user routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 
-// const userRoutes = require('./user');
+const userRoutes = require('./user');
 // const postRoutes = require('./post');
 const commentRoutes = require('./comment');
 
-// router.use('/users', userRoutes);
+router.use('/users', userRoutes);
 // router.use('/posts', postRoutes);
 router.use('/comments', commentRoutes);
 
