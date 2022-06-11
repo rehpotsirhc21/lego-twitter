2b5aff9951c36a0d4398ba9d8b54a3ada559b6ac
Blake Smethers <smethersblake@gmail.com>
2022-06-11 13:35:33 -0500

    added utils folder

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -5,12 +5,8 @@ const userRoutes = require('./user');
 const commentRoutes = require('./comment');
 const profilePicRoutes = require('./profilepic')
 
-<<<<<<< HEAD
 router.use('/profilepics', profilePicRoutes)
 // router.use('/users', userRoutes);
-=======
-router.use('/users', userRoutes);
->>>>>>> 4792cd017ad40cbe50bc06b9cc100fc310cf12b3
 // router.use('/posts', postRoutes);
 router.use("/comments", commentRoutes);
 
