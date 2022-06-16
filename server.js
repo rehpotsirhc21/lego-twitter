01e6dcb8fc479dc6cf2c4e6a252d644bb787ab44
Blake Smethers <smethersblake@gmail.com>
2022-06-15 20:12:41 -0500

    sdafg

--- server.js
+++ server.js
@@ -8,16 +8,6 @@ const SequlizeStore = require('connect-session-sequelize')(session.Store)
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sess = {
-    secret: 'Super secret secret',
-    cokkie: {},
-    resave: false,
-    saveUninitialized: true,
-    store: new SequlizeStore({
-        db: sequelize
-    })
-}
-app.use(session(sess))
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
@@ -27,21 +17,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 //need this to have access to the folder
 app.use('/uploads', express.static('uploads'));
-<<<<<<< HEAD
 app.use(session(sess))
 const hbs = exphbs.create({});
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
-=======
-app.use(require('./controllers/'));
->>>>>>> 196789c653ce0d3ce5bada4622ab8b2a79752e05
 
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
 
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}!`);
-  });
-});
+// sequelize.sync({ force: false }).then(() => {
+//   app.listen(PORT, () => {
+//     console.log(`App listening on port ${PORT}!`);
+//   });
+// });
