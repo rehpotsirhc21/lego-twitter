57949c63058001c7f9501a2fa17e460cfeed1708
Christopher Langner <langnerc@icloud.com>
2022-06-17 13:13:51 -0500

    fixed comment links. user can now view and enter comments

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -12,11 +12,26 @@ router.get('/', withAuth, (req, res) =>
     },
     attributes: [
       'id',
-      'title',
       'post_body',
-      'created_at'
+      'title',
+      'created_at',
+      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+    ],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
     ]
-    })
+  })
         .then(dbPostData =>
         {
             const posts = dbPostData.map(post => post.get({plain: true}))
