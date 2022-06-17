7637fa02b7d009b7435626e943921dbd7e1560cf
Christopher Langner <langnerc@icloud.com>
2022-06-17 14:14:58 -0500

    commiting to merge

--- controllers/api/post.js
+++ controllers/api/post.js
@@ -117,31 +117,31 @@ router.put('/vote', (req, res) => {
     }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id',  (req, res) => {
     Post.update(
-        {
-            title: req.body.title,
-            post_body: req.body.post_body
-        },
-        {
-            where: {
-                id: req.params.id
-            }
+      {
+        title: req.body.title,
+        post_body: req.body.post_body
+        
+      },
+      {
+        where: {
+          id: req.params.id
         }
-
+      }
     )
-    .then(dbPostData => {
-        if(!dbPostData) {
-            res.status(404).json({ message: 'No post found with this id!'})
-            return;
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
         }
         res.json(dbPostData);
-    })
-    .catch(err => {
+      })
+      .catch(err => {
         console.log(err);
         res.status(500).json(err);
-    });
-});
+      });
+  });
 
 router.delete('/:id', (req, res) => {
     Post.destroy({
